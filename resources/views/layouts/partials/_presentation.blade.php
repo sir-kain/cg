4759838604ec2f47c1fc7ff35cc0249f6f595ab4
Ahmadou WAly Ndiaye <ahmadouwalyndiaye@gmail.com>
2018-02-16 21:00:35 +0000

    Mise en place de la partie presentation sur la page d'accueil

--- resources/views/layouts/partials/_presentation.blade.php
+++ resources/views/layouts/partials/_presentation.blade.php
@@ -17,66 +17,34 @@
                     <div class="service-entry">
                         <img class="service-icon" src="{{asset('assets/img/service_1.png')}}" alt="">
                         <img class="service-alt" src="{{asset('assets/img/service_1_w.png')}}" alt="">
-                        <h4 class="service-title">Qui sommes-nous?</h4>
-                        <div class="service-text">Comes with Business, Festival and Music themes.</div>
+                        <h4 class="service-title">Historique</h4> <br>
+                        <div class="service-text">Sur les plans international, national, regional.</div>
                     </div>
                 </div>
                 <div class="service-block col-xs-12 col-sm-6 col-md-3">
                     <div class="service-entry">
                         <img class="service-icon" src="{{asset('assets/img/service_2.png')}}" alt="">
                         <img class="service-alt" src="{{asset('assets/img/service_2_w.png')}}" alt="">
-                        <h4 class="service-title">Historique</h4>
-                        <div class="service-text">You can choose the most suitable header for you.</div>
+                        <h4 class="service-title">Composition (Structuration)</h4>
+                        <div class="service-text">Organes, comité national, comité regional, ressources de la cellule.</div>
                     </div>
                 </div>
                 <div class="service-block col-xs-12 col-sm-6 col-md-3">
                     <div class="service-entry">
                         <img class="service-icon" src="{{asset('assets/img/service_3.png')}}" alt="">
                         <img class="service-alt" src="{{asset('assets/img/service_3_w.png')}}" alt="">
-                        <h4 class="service-title">Vision</h4>
-                        <div class="service-text">Combine different banners for each pages.</div>
+                        <h4 class="service-title">Objectifs & Ambitions</h4>
+                        <div class="service-text">Objectif général, objectifs spécifiques, perspectives</div>
                     </div>
                 </div>
                 <div class="service-block col-xs-12 col-sm-6 col-md-3">
                     <div class="service-entry">
                         <img class="service-icon" src="{{asset('assets/img/service_4.png')}}" alt="">
                         <img class="service-alt" src="{{asset('assets/img/service_4_w.png')}}" alt="">
-                        <h4 class="service-title">A propos</h4>
+                        <h4 class="service-title">Vision & Missions & Defis </h4>
                         <div class="service-text">Easy to change color by color color switcher</div>
                     </div>
                 </div>
-                <div class="service-block col-xs-12 col-sm-6 col-md-3">
-                    <div class="service-entry">
-                        <img class="service-icon" src="{{asset('assets/img/service_5.png')}}" alt="">
-                        <img class="service-alt" src="{{asset('assets/img/service_5_w.png')}}" alt="">
-                        <h4 class="service-title">Objectifs</h4>
-                        <div class="service-text">Looks amazing on all screen sizes adn across all devices.</div>
-                    </div>
-                </div>
-                <div class="service-block col-xs-12 col-sm-6 col-md-3">
-                    <div class="service-entry">
-                        <img class="service-icon" src="{{asset('assets/img/service_6.png')}}" alt="">
-                        <img class="service-alt" src="{{asset('assets/img/service_6_w.png')}}" alt="">
-                        <h4 class="service-title">compositions</h4>
-                        <div class="service-text">All fonts comes from Google Fonts Repository</div>
-                    </div>
-                </div>
-                {{--<div class="service-block col-xs-12 col-sm-6 col-md-3">
-                    <div class="service-entry">
-                        <img class="service-icon" src="{{asset('assets/img/service_7.png')}}" alt="">
-                        <img class="service-alt" src="{{asset('assets/img/service_7_w.png')}}" alt="">
-                        <h4 class="service-title">Documentation</h4>
-                        <div class="service-text">Comes with instructions on how to set up every aspect of your site</div>
-                    </div>
-                </div>
-                <div class="service-block col-xs-12 col-sm-6 col-md-3">
-                    <div class="service-entry">
-                        <img class="service-icon" src="{{asset('assets/img/service_8.png')}}" alt="">
-                        <img class="service-alt" src="{{asset('assets/img/service_8_w.png')}}" alt="">
-                        <h4 class="service-title">CSS3 Animation</h4>
-                        <div class="service-text">beautiful and smooth css3 animation</div>
-                    </div>
-                </div>  --}}
             </div>
         </div>
     </div>
